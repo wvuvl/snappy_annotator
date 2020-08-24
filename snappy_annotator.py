81878efb352efa29fe4819dcd7ffe79afc9ff595
Matthew Keaton <mrkeaton@mix.wvu.edu>
2020-08-24 12:03:46 -0400

    Updates to functionality
    - Points enlarge when hovered
    - Boxes are selected when clicked, making it easier to change the labels

--- snappy_annotator.py
+++ snappy_annotator.py
@@ -63,6 +63,7 @@ class App(anntoolkit.App):
         self.labels_on = True
         self.annotations = {}
         self.new_box = None
+        self.hovered_point = None
         self.moving_point = None
         self.hovered_box = -1
         self.moving_box = None
@@ -223,16 +224,18 @@ class App(anntoolkit.App):
     # Called once per frame. This is where things (including labels) are drawn on the image.
     def on_update(self):
         k = self.paths[self.iter]
-        self.text(k, 10, 20)
-        self.text("Metadata category: %s" % self.get_PC15_metadata_category(), 10, 50)
-        self.text("Key bindings:", 10, 140)
+        self.text(k, 10, 30)
+        self.text("Metadata category: %s" % self.get_PC15_metadata_category(), 10, 60)
+        self.text("Key bindings:", self.width-10, 30, alignment=anntoolkit.Alignment.Right)
         for i, c in enumerate(self.classes):
-            self.text("{} - {}".format(i + 1, c), 10, 170 + i * 30)
-        self.text("Current label: {}".format(self.def_label), 10, 170 + 30 * len(self.classes))
+            self.text("{} - {}".format(i + 1, c), self.width-10, 70 + i * 30, alignment=anntoolkit.Alignment.Right)
+        self.text("Current label: {}".format(self.def_label), 10, 90)
         if k in self.annotations:
-            self.text("Points count: %d" % len(self.annotations[k]), 10, 80)
+            self.text("Points count: %d" % len(self.annotations[k]), 10, 120)
             for i, p in enumerate(self.annotations[k]):
-                if i == self.moving_point:
+                if i == self.hovered_point:
+                    self.point(*p, (0, 255, 0, 250), radius=8)
+                elif i == self.moving_point:
                     self.point(*p, (0, 255, 0, 250))
                 else:
                     self.point(*p, (255, 0, 0, 250))
@@ -271,26 +274,9 @@ class App(anntoolkit.App):
         # Upon click
         if down:
             if k in self.annotations and not self.new_box:
-                points = np.asarray(self.annotations[k])
-                if len(points) > 0:
-                    point = np.asarray([[lx, ly]])
-                    d = points - point
-                    d = np.linalg.norm(d, axis=1)
-                    i = np.argmin(d)
-                    if d[i] < 6:
-                        self.moving_point = i
-                opposite_points = np.asarray(self.get_ann_opposite_corners())
-                if len(opposite_points) > 0:
-                    point = np.asarray([[lx, ly]])
-                    d = opposite_points - point
-                    d = np.linalg.norm(d, axis=1)
-                    i = np.argmin(d)
-                    if d[i] < 6:
-                        self.moving_point = i
-                        i = int(i / 2) * 2
-                        self.annotations[k][i] = opposite_points[i]
-                        self.annotations[k][i + 1] = opposite_points[i + 1]
-                if self.hovered_box >= 0 and not self.moving_point:
+                if self.hovered_point is not None:
+                    self.moving_point = self.hovered_point
+                elif self.hovered_box >= 0:
                     lower_diff = np.subtract((lx, ly), self.annotations[k][self.hovered_box * 2])
                     upper_diff = np.subtract((lx, ly), self.annotations[k][self.hovered_box * 2 + 1])
                     self.moving_box = [lower_diff, upper_diff]
@@ -298,6 +284,7 @@ class App(anntoolkit.App):
                                               self.annotations[k][self.hovered_box * 2][0]
                     self.selected_box_height = self.annotations[k][self.hovered_box * 2 + 1][1] - \
                                                self.annotations[k][self.hovered_box * 2][1]
+                    self.selected_annot = self.hovered_box
 
         # Upon release
         if not down:
@@ -315,6 +302,7 @@ class App(anntoolkit.App):
                 self.moving_point = None
                 save_to_voc_xml(k, self.path, os.getcwd(), self.database, self.get_image_dims(),
                                 self.reset_annotation_boxes(), self.labels[k])
+                self.hovered_point = None
             else:
                 self.annotations[k].append((min(max(0, lx), self.im_width), min(max(0, ly), self.im_height)))
                 if len(self.annotations[k]) % 2 == 0:
@@ -333,6 +321,7 @@ class App(anntoolkit.App):
         self.ly = ly
         k = self.paths[self.iter]
         if k in self.annotations:
+            # Dragging point
             if self.moving_point is not None:
                 self.annotations[k][self.moving_point] = (
                 min(max(0, lx), self.im_width), min(max(0, ly), self.im_height))
@@ -345,7 +334,8 @@ class App(anntoolkit.App):
                 upper_y = min(max(self.selected_box_height, ly - self.moving_box[1][1]), self.im_height)
                 self.annotations[k][self.hovered_box * 2] = (lower_x, lower_y)
                 self.annotations[k][self.hovered_box * 2 + 1] = (upper_x, upper_y)
-            elif not self.moving_box and not self.new_box:
+            elif not self.new_box:
+                # Hovering box
                 hovered_box_boxes = {}
                 boxes = [self.annotations[k][i:i + 2] for i in range(0, len(self.annotations[k]) - 1, 2)]
                 for i, b in enumerate(boxes):
@@ -357,11 +347,31 @@ class App(anntoolkit.App):
                         x = b_xmax - b_xmin
                         y = b_ymax = b_ymin
                         hovered_box_boxes[i] = x * y
-                if hovered_box_boxes:  # Finds index of the hovered box
+                if hovered_box_boxes:  # Finds index of the smallest hovered box
                     self.hovered_box = list(hovered_box_boxes.keys())[
                         list(hovered_box_boxes.values()).index(min(list(hovered_box_boxes.values())))]
                 else:
                     self.hovered_box = -1
+                # Hovering point
+                points = np.asarray(self.annotations[k])
+                opposite_points = np.asarray(self.get_ann_opposite_corners())
+                if len(opposite_points) > 0:
+                    point = np.asarray([[lx, ly]])
+                    d_p = points - point
+                    d_p = np.linalg.norm(d_p, axis=1)
+                    ind_p = np.argmin(d_p)
+                    d_op = opposite_points - point
+                    d_op = np.linalg.norm(d_op, axis=1)
+                    ind_op = np.argmin(d_op)
+                    if d_p[ind_p] < d_op[ind_op] and d_p[ind_p] < 8:
+                        self.hovered_point = ind_p
+                    elif d_op[ind_op] < d_p[ind_p] and d_op[ind_op] < 8:
+                        self.hovered_point = ind_op
+                        ind_op = int(ind_op / 2) * 2  # Round down to even number
+                        self.annotations[k][ind_op] = opposite_points[ind_op]
+                        self.annotations[k][ind_op + 1] = opposite_points[ind_op + 1]
+                    else:
+                        self.hovered_point = None
             if len(self.annotations[k]) % 2 == 1:
                 self.new_box = [
                     [self.annotations[k][-1], (min(max(0, lx), self.im_width), min(max(0, ly), self.im_height))],
